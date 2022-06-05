d1cd79e0299ab0fb64496146a957c22cec698d48
shenweijian <1834638245@qq.com>
2022-06-05 17:22:34 +0800

    加入debug

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,37 @@ const old = localStorage.getItem('user');
 if (old) {
     instance.$state = JSON.parse(old);
 }
+
+if (import.meta.env.MODE !== 'development') {
+  (function noDebuger(){
+      function testDebuger(){
+          var d=new Date();
+          debugger;
+          if(new Date()-d>10){
+              document.body.innerHTML='<div>做网站不容易，请大哥高台贵手！谢谢你</div>';
+              return true;
+          }
+          return false;
+      }
+   
+      function start(){
+          while(testDebuger()){
+              testDebuger();
+          }
+      }
+      if(!testDebuger()) {
+          window.onblur = function(){
+              setTimeout(function(){
+                  start();
+              },500)
+          }
+      }
+      else{
+          start();
+      }
+   
+  })();
+}
 </script>
 
 <style>
