7acf4481a09389c687848bd06d4ca6a204fe0cb9
shenweijian <shenweijian@huice.com>
2022-08-10 18:24:51 +0800

    格式

--- src/pages/store/index.vue
+++ src/pages/store/index.vue
@@ -20,10 +20,11 @@
         </template>
       </a-radio-group>
       <div class="shop-action">
-          <a-alert :show-icon="false">详情：{{ checkedValue.desc }}</a-alert></span>
+        <span><a-alert :show-icon="false">详情：{{ checkedValue.desc }}</a-alert></span>
+        <span>
           <span class="order-amount" v-if="checkedValue.price">￥{{ checkedValue.price }}</span>
           <span style="text-decoration:line-through;">￥{{ checkedValue.price * 2 }}</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <a-button type="primary" style="width: 200px;" @click="spon">立即赞助</a-button>
+          <a-button type="primary" style="width: 12.5rem;" @click="spon">立即赞助</a-button>
         </span>
       </div>
     </template>
@@ -97,19 +98,19 @@ getList()
 </script>
 <style lang="less" scoped>
 .arco-radio {
-  margin-bottom: 12px;
+  margin-bottom: .75rem;
 }
 
 .shop-img {
   background-image: url(https://pic4.zhimg.com/v2-ab400c5e44000df0658c6500e2e20d0f_r.jpg?source=1940ef5c);
   width: 80%;
-  height: 280px;
-  margin: 24px auto 0;
-  border-radius: 8px;
+  height: 17.5rem;
+  margin: 1.5rem auto 0;
+  border-radius: .5rem;
   background-color: #d3ddee;
-  -webkit-box-shadow: 0 0 10px 0 rgb(0 0 0 / 24%);
-  box-shadow: 0 0 10px 0 rgb(0 0 0 / 24%);
-  text-shadow: 0 0 10px rgb(0 0 82 / 0%);
+  -webkit-box-shadow: 0 0 .625rem 0 rgb(0 0 0 / 24%);
+  box-shadow: 0 0 .625rem 0 rgb(0 0 0 / 24%);
+  text-shadow: 0 0 .625rem rgb(0 0 82 / 0%);
   background-repeat: no-repeat;
   background-position: 50%;
   background-size: cover;
@@ -117,26 +118,26 @@ getList()
 
   .app-heade-title {
     color: #f0f1f5;
-    font-size: 28px;
-    padding: 60px 0 8px;
+    font-size: 1.75rem;
+    padding: 3.75rem 0 .5rem;
   }
 }
 
 .shop-box {
   display: flex;
-  min-height: 300px;
+  min-height: 18.75rem;
   width: 74%;
   box-sizing: border-box;
-  padding: 20px 40px 20px 40px;
+  padding: 1.25rem 2.5rem 1.25rem 2.5rem;
   background-color: #fff;
   position: relative;
-  margin: -136px auto 0;
+  margin: -8.5rem auto 0;
   background-color: hsla(0, 0%, 100%, .78);
-  -webkit-backdrop-filter: blur(10px);
-  backdrop-filter: blur(10px);
-  border-radius: 10px;
-  -webkit-box-shadow: 0 8px 20px 0 rgb(0 0 0 / 6%);
-  box-shadow: 0 8px 20px 0 rgb(0 0 0 / 6%);
+  -webkit-backdrop-filter: blur(.625rem);
+  backdrop-filter: blur(.625rem);
+  border-radius: .625rem;
+  -webkit-box-shadow: 0 .5rem 1.25rem 0 rgb(0 0 0 / 6%);
+  box-shadow: 0 .5rem 1.25rem 0 rgb(0 0 0 / 6%);
   flex-direction: column;
   justify-content: space-between;
 
@@ -147,46 +148,46 @@ getList()
   }
 
   .good-item {
-    width: 160px;
-    height: 80px;
-    margin-left: 30px;
+    width: 10rem;
+    height: 5rem;
+    margin-left: 1.875rem;
   }
 
   .order-amount {
-    font-size: 16px;
+    font-size: 1rem;
     color: #d64244;
     font-weight: 500;
   }
 
   .custom-radio-card-mask {
-    height: 14px;
-    width: 14px;
+    height: .875rem;
+    width: .875rem;
     display: inline-flex;
     align-items: center;
     justify-content: center;
     border-radius: 100%;
-    border: 1px solid var(--color-border-2);
+    border: .0625rem solid var(--color-border-2);
     box-sizing: border-box;
   }
 
   .custom-radio-card-mask-dot {
-    width: 8px;
-    height: 8px;
+    width: .5rem;
+    height: .5rem;
     border-radius: 100%;
   }
 
   .custom-radio-card-title {
     color: var(--color-text-1);
-    font-size: 14px;
+    font-size: .875rem;
     // font-weight: bold;
   }
 
   .custom-radio-card {
-    padding: 10px 16px;
-    border-radius: 4px;
+    padding: .625rem 1rem;
+    border-radius: .25rem;
     box-sizing: border-box;
     background-color: rgb(248, 248, 248);
-    border: 1px solid rgb(236, 236, 236);
+    border: .0625rem solid rgb(236, 236, 236);
     text-align: center;
   }
 
