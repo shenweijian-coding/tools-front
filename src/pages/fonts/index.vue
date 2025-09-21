176d1d713e3665c4cab3fa3372a18797235d5434
蓦然樱飞 <1834638245@qq.com>
2025-09-21 11:42:45 +0800

    字体展示

--- src/pages/fonts/index.vue
+++ src/pages/fonts/index.vue
@@ -7,11 +7,11 @@
     <template v-if="state.list.length">
       <div v-for="(item,index) in state.list" :key="item._id" class="list-font flex jc-between box-shadow flex-col">
         <div class="flex jc-between">
-          <span>{{ item.fontName }} - 40积分<span style="font-size: 12px;color: grey;">(来自网络搜集，若纰漏了您的字体，可联系右侧删除字体)</span></span>
+          <span>{{ item.fullName }} - 40积分<span style="font-size: 12px;color: grey;"> (来自智能程序网络采集，若纰漏了您的字体，可联系右侧微信号删除字体)</span></span>
           
           <a-button shape="round" @click="download(item)">下载字体</a-button>
         </div>
-        <img :src="`http://s3xy67004uga.xiaomiqiu.com/get-image?imageName=${state.imgs[index].file.split('.')[0]}.png`" alt="" style="height: 100px;width: 50%;">
+        <img :src="`${state.api}/get-image?imageName=${item.file.split('.')[0]}.png`" alt="" style="height: 100px;width: 50%;">
       </div>
       <div class="bg-white flex jc-center ai-center py-4 box-shadow">
         <a-pagination :total="state.total" simple :page-size="10" @change="pageChange"/>
@@ -26,11 +26,13 @@
 <script setup>
 import { getFontList, downloadFont } from '@/api/font/index'
 import Input from '@/components/Input/index.vue'
+import { Message, Modal } from '@arco-design/web-vue';
 
 const state = reactive({
   list: [],
   total: 0,
-  loading: false
+  loading: false,
+  api: null
 })
 const pageChange = (page) => {
   console.log(page);
@@ -43,6 +45,7 @@ const getList = (page = 1, name) => {
     state.list = res.data.list
     state.total = res.data.total
     state.imgs = res.data.imgs
+    state.api = res.data.api
     state.loading = false
   }).catch(err => {
     state.loading = false
@@ -56,7 +59,11 @@ const download = (item) => {
   state.loading = true
   downloadFont({ fontId: item._id }).then(res => {
     state.loading = false
-    window.open(res.data,'_blank')
+    if(res.data.indexOf('http') != -1) {
+      window.open(res.data,'_blank')
+    } else {
+      Message.warning(res.data)
+    }
   }).catch(err => {
     state.loading = false
   })
